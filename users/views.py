2fe81978050e0d7f72b55851e35f8581d1af0797
Svyatoslav Deinekin <svyadey@mail.ru>
2024-04-29 23:41:21 +0300

    django hw7 fixed

--- users/views.py
+++ users/views.py
@@ -1,14 +1,15 @@
 import secrets
 import random
+from django.shortcuts import render
 
 from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 
 from config.settings import EMAIL_HOST_USER
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UpdatePasswordForm
 from users.models import User
 
 
@@ -34,7 +35,7 @@ class UserCreateView(CreateView):
         return super().form_valid(form)
 
 
-def email_verification(token):
+def email_verification(request, token):
     user = get_object_or_404(User, token=token)
     user.is_active = True
     user.save()
@@ -42,17 +43,44 @@ def email_verification(token):
 
 
 def update_password(request):
-    new_password = ''.join([str(random.randint(0, 9)) for _ in range(16)])
-    new_password = make_password(new_password)
-    send_mail(
-        subject="Новый пароль",
-        message=f"Новый пароль для входа: {new_password}",
-        from_email=EMAIL_HOST_USER,
-        recipient_list=[request.user.email]
-    )
-    request.user.set_password(new_password)
-    request.user.save()
-    return redirect(reverse('users:login'))
+    return render(request, 'users/update_password.html')
 
 
+def generate_and_send_password(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        user = User.objects.get(email=email)
+        new_password = make_password(''.join(str(random.randint(0, 9)) for _ in range(16)))
+        user.set_password(new_password)
 
+        send_mail(
+            subject="Новый пароль",
+            message=f"Новый пароль для входа: {new_password}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        user.save()
+
+    return render(request, 'users/update_password.html')
+
+
+class UpdatePassword(FormView):
+    form_class = UpdatePasswordForm
+    template_name = 'users/update_password.html'
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            user = User.objects.get(email=email)
+            new_password = ''.join(str(random.randint(0, 9)) for _ in range(16))
+            user.password = make_password(new_password)
+            send_mail(
+                subject='Вы сменили пароль',
+                message=f'Ваш новый пароль {new_password}',
+                from_email=EMAIL_HOST_USER,
+                recipient_list=[user.email],
+            )
+            user.save()
+
+        return super().form_valid(form)
